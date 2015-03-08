bdd5720ea7d47634619c6be48b053080765dacf6
Galleythrey <njutangshanshan@outlook.com>
2015-03-08 21:25:54 +0800

    用户注册&登陆验证数据库函数
    这次用到的常量（数据库服务器地址，用户名，登陆密码，数据库）写在constants.php中了，用到这些的时候请使用这些常量方便以后统一修改。再有什么需要的常量也请添加到这个文件里，以上~

---
+++ etc/constants.php
@@ -0,0 +1,6 @@
+﻿<?php
+	define("dbServer","127.0.0.1");		//数据库服务器
+	define("dbUserName","niconiconi");	//数据库用户
+	define("dbPassword","12345");		//数据库登陆密码
+	define("database","wehelp");		//数据库名称
+?>
---
+++ etc/register.php
@@ -0,0 +1,49 @@
+﻿<?php
+	require('constants.php');
+
+	//注册
+	function register($user,$openID,$stuNum,$mail){
+		
+		//验证数据库服务器连接
+		if(($con=mysql_connect(dbServer,dbUserName,dbPassword))===FALSE)
+			die("could not conect to database");
+		
+		//验证服务器连接
+		if(mysql_select_db(database)===FALSE)
+			die("could not conect to database");
+		mysql_query("set names 'utf8'");	
+		
+		//插入注册信息
+		$sql_insert = mysql_query("insert into user (user,openID,stuNum,mailBox)values('$user','$openID','$stuNum','$mail')");
+
+	}
+
+	//登陆验证
+	function login($varopenID){
+		
+		
+		//验证数据库服务器连接
+		if(($con=mysql_connect(dbServer,dbUserName,dbPassword))===FALSE)
+			die("could not conect to database");
+		
+		//验证服务器连接
+		if(mysql_select_db(database)===FALSE)
+			die("could not conect to database");
+
+		//查找数据库中是否含有该ID
+		$sql = "select 1 from user where openID = '$varopenID'";
+		$result = mysql_query($sql);
+		
+		//验证数据库操作是否成功
+		if($result===FALSE)
+			die("could not query database");
+		
+		//若该用户已注册，返回true，否则返回假
+		if(mysql_num_rows($result)==1)
+			return TRUE;
+		else
+			return FALSE;
+	}
+	$result = login("zxcvb");
+	echo "$result";
+?>
