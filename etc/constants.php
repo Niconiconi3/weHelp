7865beb1a34abfa1b105432f6cdeec0406ade1bf
Mao-xy <15850563830@139.com>
2015-03-11 16:30:56 +0800

    添加常量&文本过滤
    常量加了初始积分和任务置顶消耗积分

--- etc/constants.php
+++ etc/constants.php
@@ -3,4 +3,6 @@
 	define("dbUserName","niconiconi");	//数据库用户
 	define("dbPassword","12345");		//数据库登陆密码
 	define("database","wehelp");		//数据库名称
+	define("ini_points",20);            //初始积分
+	define("stickie_points",3);         //置顶扣除积分
 ?>
