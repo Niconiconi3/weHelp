f4d38ad4c2e59bac7ab67cd44d6f0854914b9071
Mao-xy <15850563830@139.com>
2015-03-19 10:36:36 +0800

    增加修改有效期和积分

--- etc/editTask.php
+++ etc/editTask.php
@@ -1,14 +1,18 @@
 <?php
 /*
-此段代码用于编辑任务，包括修改内容，关闭任务，置顶任务
+此段代码用于编辑任务，包括修改内容，修改悬赏积分，修改有效期，关闭任务，置顶任务
 需要使用$action(可选值为modify_task,close_task,stickie_task),$content,$points,$有效期，$task_no,$studentid
 */
 	include_once("constants.php");
 	
     $action = $_POST['action'];
 	$task_id = $_POST['task_id'];
-	if($action=='modify_Content'){
+	if($action=='modify_content'){
 		modify_content();
+	}elseif($action=='modify_points'){
+		modify_points();
+	}elseif($action=='modify_date'){
+		modify_date();
 	}elseif($action=='close_task'){
 		close_task();
 	}elseif($action=='stickie_task'){
@@ -25,7 +29,7 @@
 		if(mysql_select_db(database)===FALSE)
 			die("could not connect to database");
 	    mysql_query("set names 'utf8'");
-		$sql = "UPDATE `task` SET `task_content` = $content WHERE `id` = $task_id";
+		$sql = "UPDATE `task` SET `content` = $content WHERE `ID` = $task_id";
 		$result = mysql_query($sql);
 		if(!$result){
 			echo '修改任务内容失败，请稍后尝试';
@@ -35,6 +39,43 @@
 		}
 	}
 	
+	function modify_points(){
+		$points = $_POST['points'];
+		$con = mysql_connect(dbServer,dbUserName,dbPassword);
+        if (!$con){
+			die('Could not connect: ' . mysql_error());
+		}
+		if(mysql_select_db(database)===FALSE)
+			die("could not connect to database");
+	    mysql_query("set names 'utf8'");
+		$sql = "UPDATE `task` SET `point` = $points WHERE `ID` = $task_id";
+		$result = mysql_query($sql);
+		if(!$result){
+			echo '修改失败，请稍后尝试';
+		}
+		else{
+			echo '任务修改成功！';
+		}
+	}
+	
+	function modify_date(){
+		$date = $_POST['date'];
+		$con = mysql_connect(dbServer,dbUserName,dbPassword);
+        if (!$con){
+			die('Could not connect: ' . mysql_error());
+		}
+		if(mysql_select_db(database)===FALSE)
+			die("could not connect to database");
+	    mysql_query("set names 'utf8'");
+		$sql = "UPDATE `task` SET `validity` = $date WHERE `ID` = $task_id";
+		$result = mysql_query($sql);
+		if(!$result){
+			echo '修改失败，请稍后尝试';
+		}
+		else{
+			echo '任务修改成功！';
+		}
+	}
 	function close_task(){
 		$con = mysql_connect(dbServer,dbUserName,dbPassword);
         if (!$con){
